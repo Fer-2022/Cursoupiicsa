b1862fb06f459290e7cd76c3e2f6f31835179e33
Fer-2022 <105828084+Fer-2022@users.noreply.github.com>
2022-05-18 19:51:03 -0500

    Primera subida de archivo

---
+++ Curso upiicsa/Holamundo.py
@@ -0,0 +1,11 @@
+#COMENTARIO: Saludo primer programa
+#los comentarios nos sirven para documentar y describir cada parte del program
+#los comentarios no los toma en cuenta o los omite python
+
+
+print("Hola mundo")
+print('Fernando Rangel')
+print(5+2)
+print(10-5)
+print(8*3)
+print(20/5)
