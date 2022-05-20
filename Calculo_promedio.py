4c68978ded1e9b743de67307f94cb6fe1935484c
Fer-2022 <105828084+Fer-2022@users.noreply.github.com>
2022-05-19 20:05:45 -0500

    Add files via upload

---
+++ Calculo_promedio.py
@@ -0,0 +1,15 @@
+#Entrada de datos
+
+número1= float(input("Digite el valor de la calificacion del 1er departamental:"))
+número2= float(input("Digite el valor de la calificacion del 2do departamental:"))
+número3=  float(input("Digite el valor de la calificacion del 3er departamental:"))
+
+
+
+#PROCESOS(calculos, operaciones)
+
+promedio= (número1 + número2 + número3) / 3
+
+#SALIDA DE DATOS 
+
+print ("Tu promedio es igual a=", promedio)
