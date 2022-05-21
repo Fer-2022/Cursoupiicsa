dd40c7d7fc55f679dd6951ec2ab6ee5d59579249
Fer-2022 <105828084+Fer-2022@users.noreply.github.com>
2022-05-20 19:08:01 -0500

    Add files via upload

---
+++ Edad_Person.py
@@ -0,0 +1,12 @@
+#ENTRADA DE DATOS
+
+número1 = float(input("Digita el año en el que naciste: "))
+número2 = float(input("Digita el año actual: "))
+
+#PROCESOS(Calculos u operaciones Matematicas y la logica)
+
+edad = (número2 - número1)
+
+#SALIDA DE DATOS
+
+print("La edad actual de la persona es igual a=", edad)
