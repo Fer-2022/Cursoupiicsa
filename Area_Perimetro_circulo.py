9ed0cfc1f50c3df0524b9ed79ece3c56e8b08d33
Fer-2022 <105828084+Fer-2022@users.noreply.github.com>
2022-05-20 19:08:22 -0500

    Add files via upload

---
+++ Area_Perimetro_circulo.py
@@ -0,0 +1,14 @@
+#ENTRADA DE DATOS 
+
+radio = float(input("Digita el valor del radio:"))
+PI = 3.1416
+
+#PROCESOS (Cálculos u operaciones Matematicas y la logica)
+
+área = (PI * radio ** 2)
+perímetro = (2 * PI * radio)
+
+#SALIDA DE DATOS
+
+print("El AREA del circulo es igual a=", área)
+print("El PERIMETRO del circulo es igual a=", perímetro)
